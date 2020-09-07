69f37755bd5e5c66e89e45d7eeac22b64965b166
emmeem <1124782639@qq.com>
2020-09-07 21:05:29 +0800

    getStudentGroups

--- src/main/java/com/thoughtworks/capability/gtb/entrancequiz/Controller/StudentController.java
+++ src/main/java/com/thoughtworks/capability/gtb/entrancequiz/Controller/StudentController.java
@@ -1,17 +1,30 @@
 package com.thoughtworks.capability.gtb.entrancequiz.Controller;
 
+import com.thoughtworks.capability.gtb.entrancequiz.Domain.Group;
 import com.thoughtworks.capability.gtb.entrancequiz.Domain.Student;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import com.thoughtworks.capability.gtb.entrancequiz.Utils.InitStudentList;
+import com.thoughtworks.capability.gtb.entrancequiz.Utils.StudentList;
+
 import java.util.List;
 
 @RestController
 public class StudentController {
 
     @GetMapping("/students")
+    @CrossOrigin
     public List<Student> getStudentList() {
-        InitStudentList initStudentList = new InitStudentList();
-        return initStudentList.initStudent();
+        StudentList initStudentList = new StudentList();
+        return initStudentList.initStudentList();
     }
+
+    @GetMapping("/groups")
+    @CrossOrigin
+    public List<Group> getGroups() {
+        StudentList groups = new StudentList();
+        return groups.getStudentGroups();
+    }
+
+
 }
---
+++ src/main/java/com/thoughtworks/capability/gtb/entrancequiz/Utils/StudentList.java
@@ -0,0 +1,56 @@
+package com.thoughtworks.capability.gtb.entrancequiz.Utils;
+
+import com.thoughtworks.capability.gtb.entrancequiz.Domain.Group;
+import com.thoughtworks.capability.gtb.entrancequiz.Domain.Student;
+
+import java.util.*;
+
+public class StudentList {
+    String[] studentName = {"沈乐棋","徐慧慧","陈思聪","王江林","王登宇","杨思雨","江雨舟","廖燊","胡晓","但杰","盖迈达",
+            "肖美琦","黄云洁","齐瑾浩","刘亮亮","肖逸凡","王作文",
+            "郭瑞凌", "李明豪", "党泽", "肖伊佐", "贠晨曦", "李康宁", "马庆", "商婕", "余榕",
+            "谌哲", "董翔锐", "陈泰宇", "赵允齐", "张柯", "廖文强", "刘轲", "廖浚斌", "凌凤仪"};
+    public List<Student> initStudentList() {
+        List<Student> studentList = new ArrayList<>();
+        for(int i=0; i<35; i++) {
+            Student s1 = new Student();
+            s1.setId(i+1);
+            s1.setStudentName(studentName[i]);
+            studentList.add(s1);
+        }
+        return studentList;
+    }
+
+    public List<Group> getStudentGroups() {
+        List<Student> student = initStudentList();
+        Collections.shuffle(student);
+        int stuSize =student.size();
+        int initGroup = stuSize/6;
+        int restStudents = 0;
+        if(initGroup*6 < stuSize) {
+            restStudents = stuSize - initGroup*6;
+        }
+        List<Group> studentGroup = new ArrayList<>();
+        for(int i=0; i<6; i++) {
+            List<Student> mid = new ArrayList<>();
+            for(int j=0; j<initGroup; j++) {
+                mid.add(student.get(i*initGroup+j));
+            }
+            Group tmp = new Group("Group"+(i+1),mid);
+            studentGroup.add(tmp);
+        }
+        if(restStudents > 0) {
+            for(int i=0; i<6; i++) {
+                List<Student> mid = new ArrayList<>();
+                for(int j=0; j<initGroup; j++) {
+                    mid.add(student.get(i*initGroup+j));
+                }
+                Group tmp = new Group("Group"+(i+1),mid);
+                studentGroup.add(tmp);
+            }
+            restStudents -= 1;
+        }
+
+        return studentGroup;
+    }
+}
